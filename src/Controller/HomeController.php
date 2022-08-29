e80a93111ecb1af00a05d5be92d76f6ca46884ac
davydebut <spanudavy@gmail.com>
2022-08-29 16:50:00 +0200

    addflash connection

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,42 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (new Email())
+                ->from($data['EmailAddress'])
+                ->to('davylea.c@gmail.com')
+                ->subject('Message de ' . $data['FirstName'])
+                ->text($data['YourMessage'])
+                ->html('<p>' . $data['YourMessage'] . '</p>');
+
+            $mailer->send($email);
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'LEA.C COIFFURE STUDIO',
             'title' => 'BIENVENUE À',
             'description' => 'LEA.C COIFFURE STUDIO',
             'message' => 'le meilleur endroit pour une nouvelle',
             'suite' => 'coiffure raffraichissante !',
+            'contact' => $form->createView(),
         ]);
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,7 +40,9 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('home');
+            $this->addFlash('success', 'Inscription réussie !');
+
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
