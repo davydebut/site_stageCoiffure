fe31947a0668f7bb331a821166631418266a6dee
davydebut <spanudavy@gmail.com>
2022-08-30 16:47:23 +0200

    CSS LOGIN

--- public/assets/app.js
+++ public/assets/app.js
@@ -4,4 +4,16 @@ $(function () {
       $nav.toggleClass('scrolled p-2', $(this).scrollTop() > $nav.height());
       $nav.toggleClass('ms-5', $(this).scrollTop() < $nav.height());
     });
+    $("#show_hide_password a").on('click', function (event) {
+      event.preventDefault();
+      if ($('#show_hide_password input').attr("type") == "text") {
+          $('#show_hide_password input').attr('type', 'password');
+          $('#show_hide_password i').addClass("fa-eye-slash");
+          $('#show_hide_password i').removeClass("fa-eye");
+      } else if ($('#show_hide_password input').attr("type") == "password") {
+          $('#show_hide_password input').attr('type', 'text');
+          $('#show_hide_password i').removeClass("fa-eye-slash");
+          $('#show_hide_password i').addClass("fa-eye");
+      }
+  });
   });
